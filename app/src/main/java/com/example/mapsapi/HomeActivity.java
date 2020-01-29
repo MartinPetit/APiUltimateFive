84c69ecf3273ec6e377b1f8dc9440a78b88b101a
MartinPetit <martinpetit1998@gmail.com>
2020-01-29 19:00:02 +0100

    caroussellpicker

--- app/src/main/java/com/example/mapsapi/HomeActivity.java
+++ app/src/main/java/com/example/mapsapi/HomeActivity.java
@@ -10,6 +10,11 @@ import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import in.goodiebag.carouselpicker.CarouselPicker;
+
 public class HomeActivity extends AppCompatActivity {
     private int[] mImages = new int[] {
         R.drawable.mbappe, R.drawable.chaussures, R.drawable.easy, R.drawable.five, R.drawable.ronaldo
@@ -18,6 +23,8 @@ public class HomeActivity extends AppCompatActivity {
     private String[] mImagesTitle = new String[] {
         "mbappe", "chaussures", "easy", "five", "ronaldo"
     };
+
+    CarouselPicker carouselPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +46,19 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(HomeActivity.this, mImagesTitle[position], Toast.LENGTH_SHORT).show();
             }
         });
+
+        carouselPicker = (CarouselPicker) findViewById(R.id.carouselPicker);
+
+
+        List<CarouselPicker.PickerItem> mixItems = new ArrayList<>();
+        mixItems.add(new CarouselPicker.TextItem("One", 20));
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.ronaldo));
+        mixItems.add(new CarouselPicker.TextItem("two", 20));
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.mbappe));
+        CarouselPicker.CarouselViewAdapter mixAdapter = new CarouselPicker.CarouselViewAdapter(this, mixItems,0);
+        carouselPicker.setAdapter(mixAdapter);
+
+
+
     }
 }
